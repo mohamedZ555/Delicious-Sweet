4f709d15ba740ad2a7d0f01be330ce9e9938d945
Mariem Ibrahim <53022371+MariemIbrahimBadawy@users.noreply.github.com>
2025-04-03 16:47:03 +0200

    done
    login and register

--- delicious-sweet/app/component/footer.jsx
+++ delicious-sweet/app/component/footer.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 export default function Footer() {
     return (
@@ -5,12 +6,13 @@ export default function Footer() {
         <div className="container text-center">
           <p className="mb-1">© 2025 Delicious Sweet. All rights reserved.</p>
           <div>
-            <a href="/privacy" className="text-light mx-2">Privacy Policy</a>
-            <a href="/terms" className="text-light mx-2">Terms of Service</a>
-            <a href="/contact" className="text-light mx-2">Contact Us</a>
+            <Link href="#" className="text-light mx-2">Privacy Policy</Link>
+            <Link href="#" className="text-light mx-2">Terms of Service</Link>
+            <Link href="/login" className="text-light mx-2">Contact Us</Link>
           </div>
         </div>
       </footer>
+      
     );
   }
   
--- delicious-sweet/app/login/page.jsx
+++ delicious-sweet/app/login/page.jsx
@@ -1,8 +1,25 @@
+import Link from "next/link";
 
 export default function Login() {
     return (
-      <div>
-         login
+      <div className="container  my-4">
+        <form className="row g-3">
+          <div className="col-12">
+            <label for="inputEmail4" className="form-label">Email</label>
+            <input type="email" className="form-control" id="inputEmail4"/>
+          </div>
+          <div className="col-12">
+            <label for="inputPassword4" className="form-label">Password</label>
+            <input type="password" className="form-control" id="inputPassword4"/>
+          </div>
+          <div className="col-12">
+            <button type="submit" className="btn btn-primary">Sign in</button>
+          </div>
+          <div className="col-12">
+            <p>If you are not <Link href="register">Sign Up </Link>? </p>
+            
+          </div>
+        </form>
   
       </div>
     );
