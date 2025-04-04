e93217a0d20e4c8ffd72e4df15494beabcd13c38
mohamed ashraf <mohamed.ashraff877@gmail.com>
2025-04-04 17:26:57 +0200

    hh

---
+++ delicious-sweet/app/not-found.jsx
@@ -0,0 +1,8 @@
+
+export default function Error() {
+  return (
+    <div style={{height: "100vh"}} className="d-flex justify-content-center align-items-center flex-column">
+        <div>not found</div>
+    </div>
+  );
+}
