96f684cc3e5e04021c2849a7d202ec210177dc1c
Mariem Ibrahim <53022371+MariemIbrahimBadawy@users.noreply.github.com>
2025-04-07 21:49:23 +0200

    fgdfg

--- delicious-sweet/app/register/page.jsx
+++ delicious-sweet/app/register/page.jsx
@@ -13,6 +13,14 @@ export default function Register() {
           </h2>
         <form className="row g-3">
           <div className="col-md-6">
+            <label for="inputFName" className="form-label">First Name</label>
+            <input type="text" className="form-control" id="inputFName"/>
+          </div>
+          <div className="col-md-6">
+            <label for="inputLName" className="form-label">Last Name</label>
+            <input type="text" className="form-control" id="inputLName"/>
+          </div>
+          <div className="col-md-12">
             <label for="inputEmail4" className="form-label">Email</label>
             <input type="email" className="form-control" id="inputEmail4"/>
           </div>
@@ -20,33 +28,22 @@ export default function Register() {
             <label for="inputPassword4" className="form-label">Password</label>
             <input type="password" className="form-control" id="inputPassword4"/>
           </div>
+          <div className="col-md-6">
+            <label for="inputPassword4" className="form-label">Confirm Password</label>
+            <input type="password" className="form-control" id="inputPassword4"/>
+          </div>
+          <div className="col-md-12">
+            <label for="inputPhone" className="form-label">Phone Number</label>
+            <input type="number" className="form-control" id="inputPhone"/>
+          </div>
           <div className="col-12">
             <label for="inputAddress" className="form-label">Address</label>
             <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St"/>
           </div>
-          <div className="col-12">
-            <label for="inputAddress2" className="form-label">Address 2</label>
-            <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
-          </div>
-          <div className="col-md-6">
-            <label for="inputCity" className="form-label">City</label>
-            <input type="text" className="form-control" id="inputCity"/>
-          </div>
-          <div className="col-md-4">
-            <label for="inputState" className="form-label">State</label>
-            <select id="inputState" className="form-select">
-              <option selected>Choose...</option>
-              <option>...</option>
-            </select>
-          </div>
          
           <div className="col-12">
-            <div className="form-check">
-              <input className="form-check-input" type="checkbox" id="gridCheck"/>
-              <label className="form-check-label" for="gridCheck">
-                Check me out
-              </label>
-            </div>
+            <label for="inputCity" className="form-label">City</label>
+            <input type="text" className="form-control" id="inputCity"/>
           </div>
           <div className="col-12">
             <button type="submit" className=" btn btn-submit btn-animated">Sign Up</button>
